dc1c6ff53d2f716629e7c963a69590742adf1d3b
roseranijohn <roseranijohn@users.noreply.github.com>
2023-01-17 06:22:18 +0530

    Add files via upload

---
+++ src/pkg/Square.java
@@ -0,0 +1,12 @@
+package pkg;
+import test.Rectangle;
+public class Square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Rectangle r1=new Rectangle();
+		System.out.println(r1.getArea());
+
+	}
+
+}
